cbcc2ad2eb2326d70e03d4729d5f469620fee01d
zihanlim <zihan0804@gmail.com>
2023-09-09 22:28:24 +0800

    update

---
+++ springboot-java-project-sctp-module3/src/main/java/sg/ntu/edu/springbootjavaprojectsctpmodule3/SpringbootJavaProjectSctpModule3Application.java
@@ -0,0 +1,13 @@
+package sg.ntu.edu.springbootjavaprojectsctpmodule3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJavaProjectSctpModule3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJavaProjectSctpModule3Application.class, args);
+	}
+
+}
---
+++ springboot-java-project-sctp-module3/src/test/java/sg/ntu/edu/springbootjavaprojectsctpmodule3/SpringbootJavaProjectSctpModule3ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.ntu.edu.springbootjavaprojectsctpmodule3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJavaProjectSctpModule3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
